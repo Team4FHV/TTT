73203defb70d57cc04041c0d19c56ec6ba453927
Steve2301 <stefan.dietrich@students.fhv.at>
2013-11-11 11:30:21 +0100

    GUI: KundeAnlegen GUI-CTRL: KundeAnlegenCtrl
    Erstellung

---
+++ src/GUIController/KundeAnlegenCtrl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUIController;
+
+/**
+ *
+ * @author Stefan Dietrich
+ */
+public class KundeAnlegenCtrl {
+
+    public void neuenKundeAnlegen(String text, String text0, String text1, String text2, String text3, String text4, String text5, String text6, String text7, String text8, String text9, String text10) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

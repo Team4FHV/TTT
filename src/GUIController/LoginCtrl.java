f3d729d6442c33d9e5dd8e683a7d518005b83db6
MonikaBuergi <monika.buergi@students.fhv.at>
2013-11-11 06:59:31 +0100

    neue Files: Login.form, Login.java. LoginCtrl.java Selection.form, Selection.java DTOBenutzername.java

---
+++ src/GUIController/LoginCtrl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUIController;
+
+import DTO.objecte.DTOBenutzername;
+/**
+ *
+ * @author Monika
+ */
+public class LoginCtrl {
+    private DTOBenutzername benutzername;
+}

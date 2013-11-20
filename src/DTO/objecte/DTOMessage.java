81c9df9ba0e46560feae000c266d88aa29fa5a3c
MonikaBuergi <monika.buergi@students.fhv.at>
2013-11-20 14:38:47 +0100

    DTOMessage: Topic hinzugefügt

--- src/DTO/objecte/DTOMessage.java
+++ src/DTO/objecte/DTOMessage.java
@@ -15,6 +15,7 @@ public class DTOMessage {
     private String _text;
     private Date _date;
     private int _id;
+    private String _topic; 
 
     public DTOMessage() {        
     }
@@ -25,37 +26,45 @@ public class DTOMessage {
         _date = date;
         _id = id;
     }
+
+    public String getTopic() {
+        return _topic;
+    }
+
+    public void setTopic(String topic) {
+        this._topic = topic;
+    }
     
     public String getTitle() {
         return _title;
     }
 
-    public void setTitle(String _title) {
-        this._title = _title;
+    public void setTitle(String title) {
+        this._title = title;
     }
 
     public String getText() {
         return _text;
     }
 
-    public void setText(String _text) {
-        this._text = _text;
+    public void setText(String text) {
+        this._text = text;
     }
 
     public Date getDate() {
         return _date;
     }
 
-    public void setDate(Date _date) {
-        this._date = _date;
+    public void setDate(Date date) {
+        this._date = date;
     }
 
     public int getId() {
         return _id;
     }
 
-    public void setId(int _id) {
-        this._id = _id;
+    public void setId(int id) {
+        this._id = id;
     }
     
     

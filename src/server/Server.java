63fa7b04bc5ca7ce3794b0df115fb4637a9bb8c3
Steve2301 <stefan.dietrich@students.fhv.at>
2013-12-10 10:39:18 +0100

    Zertifikat für LDAP eingebunden

--- src/server/Server.java
+++ src/server/Server.java
@@ -14,30 +14,29 @@ import java.rmi.registry.Registry;
 public class Server {
 
     public static void main(String[] args) {
-            System.out.println("Hallo");
-           
-            try { //special exception handler for registry creation
-            LocateRegistry.createRegistry(Registry.REGISTRY_PORT); 
+        System.out.println("Hallo");
+        String _cacert = System.getProperty("java.home") + "/lib/security/cacerts";
+        System.setProperty("javax.net.ssl.trustStore", _cacert);
+        try { //special exception handler for registry creation
+            LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
             System.out.println("java RMI registry created.");
         } catch (RemoteException e) {
             //do nothing, error means registry already exists
             System.out.println("java RMI registry already exists.");
         }
-             
 
         try {
-             
+
             RMIControllerFactory rmiControllerFactory = new RMIControllerFactory();
             // Bind this object instance to the name "RMIControllerFactoryObject"
-            
+
             Naming.rebind("rmi://localhost/RMIControllerFactoryObject", rmiControllerFactory);
-           
+
             System.out.println("RMI Object bound in registry");
-            
 
         } catch (RemoteException | MalformedURLException exc) {
             System.out.println(exc.getMessage());
         }
-        
+
     }
 }
